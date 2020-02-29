419c70b7dc952eb2ba74474143a9a072add3d3db
Luis David Morales <luisdavidmorales95@gmail.com>
2020-02-28 19:05:23 -0600

    Rutas parte 1

--- 3-angular-intro/my-app/src/app/app.module.ts
+++ 3-angular-intro/my-app/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,16 +7,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { Body2Component } from './body2/body2.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+
+  { path: 'contacto', component: ContactComponent }
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
     FooterComponent,
+    BodyComponent,
+    Body2Component,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
